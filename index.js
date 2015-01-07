378c5bf2cf459c202ed354246ffdbf7ddd1aa6f0
Rob Lum <lum.robert@gmail.com>
2015-01-07 15:27:08 -0500

    sql select * from test working

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 var express = require('express');
 var app = express();
 var mysql      = require('mysql');
-
-console.log('mysql', mysql);
 var connection = mysql.createConnection({
   host     : 'us-cdbr-iron-east-01.cleardb.net',
   user     : 'b1d6e2f45de4e9',
   password : 'ee212855',
+  database : 'heroku_b060aa6180054ee'
 });
 
 connection.connect();
 
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
+connection.query('SELECT * FROM heroku_b060aa6180054ee.test', function(err, result) {
+     if (err) throw err;
 
-  console.log('The solution is: ', rows[0].solution);
+     console.log(result);
 });
 
 connection.end();
