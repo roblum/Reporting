b0c6ecbb3404127f407fa884753d2bd94f601d2a
Rob Lum <lum.robert@gmail.com>
2015-01-08 12:40:49 -0500

    moved db queries into separate file; require into index.js; working on postman

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express         = require('express');
 var app             = express();
 var sql_connect     = require('./controllers/sql_connection.js');
+var DB        = require('./controllers/db_queries.js');
 var cors            = require('cors');
 
 app.set('port', (process.env.PORT || 5000));
@@ -10,48 +11,10 @@ app.post('/report', cors(), function(request, response) {
      response.send('Hello World!');
 
      var connection = sql_connect.connect();
-     var reqDomain = request.query.domain
+     var reqDomain  = request.query.domain
 
-     searchDB(reqDomain, connection);
+     DB.searchDB(reqDomain, connection);
 });
-
-function searchDB(reqDomain, connection){
-     var select = 'SELECT id FROM heroku_b060aa6180054ee.reporting WHERE Domain = "' + reqDomain + '"'
-
-     connection.query(select, function(err, result) {
-          if (err) throw err;
-          console.log('result', result);
-
-          if (!result[0]){
-               console.log('no results');
-               writeToDB(reqDomain, connection);
-          } else {
-               var id = result[0].id;
-               console.log('increment result');
-               increment(reqDomain, connection, id);
-          }
-     });
-}
-
-function writeToDB(reqDomain, connection){
-     var insertion = 'INSERT into heroku_b060aa6180054ee.reporting(domain, count) VALUES ("' + reqDomain + '", 1)';
-
-     connection.query(insertion, function(err, result){
-          if (err) throw err;
-
-          connection.end();
-     });
-}
-
-function increment(reqDomain, connection, id){
-     var update = 'UPDATE reporting SET Count = Count + 1 WHERE id = ' + id
-
-     connection.query(update, function(err, result){
-          if (err) throw err;
-
-          connection.end();
-     });
-}
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
