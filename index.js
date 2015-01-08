958580923febdfb40c9ce8fe0308902a082a2352
Rob Lum <lum.robert@gmail.com>
2015-01-08 12:42:10 -0500

    formatting

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express         = require('express');
 var app             = express();
 var sql_connect     = require('./controllers/sql_connection.js');
-var DB        = require('./controllers/db_queries.js');
+var DB              = require('./controllers/db_queries.js');
 var cors            = require('cors');
 
 app.set('port', (process.env.PORT || 5000));
@@ -17,7 +17,7 @@ app.post('/report', cors(), function(request, response) {
 });
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+     console.log("Node app is running at localhost:" + app.get('port'));
 });
 
 
