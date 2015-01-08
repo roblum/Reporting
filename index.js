720b27c12508e5fcd7653be7bbfd58bb785f1ab9
Rob Lum <lum.robert@gmail.com>
2015-01-08 11:43:36 -0500

    working on local; increment & insert working, need to reconfigure auto increment by 1 instead of 10

--- index.js
+++ index.js
@@ -1,35 +1,60 @@
-var express    = require('express');
-var app        = express();
-var mysql      = require('mysql');
-var url        = require('url');
-var cors       = require('cors');
-
-var connection = mysql.createConnection({
-  host     : 'us-cdbr-iron-east-01.cleardb.net',
-  user     : 'b1d6e2f45de4e9',
-  password : 'ee212855',
-  database : 'heroku_b060aa6180054ee'
-});
+var express         = require('express');
+var app             = express();
+var sql_connect     = require('./sql_connection.js');
+var cors            = require('cors');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.post('/report', cors(), function(request, response) {
-  response.send('Hello World!');
+     response.send('Hello World!');
 
-     connection.connect();
+     var connection = sql_connect.connect();
      var reqDomain = request.query.domain
-     console.log(reqDomain);
-     var insertion = 'INSERT into heroku_b060aa6180054ee.reporting(id, domain, count) VALUES (1, ' + reqDomain + ', 2)';
 
-     connection.query(insertion, function(err, result) {
+     searchDB(reqDomain, connection);
+});
+
+function searchDB(reqDomain, connection){
+     var select = 'SELECT id FROM heroku_b060aa6180054ee.reporting WHERE Domain = "' + reqDomain + '"'
+
+     connection.query(select, function(err, result) {
+          if (err) throw err;
+          console.log('result', result);
+
+          if (!result[0]){
+               console.log('no results');
+               writeToDB(reqDomain, connection);
+          } else {
+               var id = result[0].id;
+               console.log('increment result');
+               increment(reqDomain, connection, id);
+          }
+     });
+}
+
+function writeToDB(reqDomain, connection){
+     var insertion = 'INSERT into heroku_b060aa6180054ee.reporting(domain, count) VALUES ("' + reqDomain + '", 1)';
+
+     connection.query(insertion, function(err, result){
           if (err) throw err;
 
+          connection.end();
      });
+}
 
-     connection.end();
-});
+function increment(reqDomain, connection, id){
+     var update = 'UPDATE reporting SET Count = Count + 1 WHERE id = ' + id
+
+     connection.query(update, function(err, result){
+          if (err) throw err;
+
+          connection.end();
+     });
+}
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
+
+
