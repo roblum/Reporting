1037364bd92aaef43954e1ae06aac315120b56d2
Rob Lum <lum.robert@gmail.com>
2015-01-13 16:09:58 -0500

    this file will pull all rows from sql db and create a report posted to s3 bucket

--- report.js
+++ report.js
@@ -1,1 +1,34 @@
-console.log(process.env.S3_BUCKET);
+var AWS             = require('aws-sdk');
+var S3_key          = process.env.S3_KEY;
+var S3_secret       = process.env.S3_SECRET;
+var S3_bucket       = process.env.S3_BUCKET;
+var sql_connect     = require('./controllers/sql_connection.js');
+var DB              = require('./controllers/db_queries.js');
+
+AWS.config.update({
+               accessKeyId: S3_key,
+               secretAccessKey: S3_secret,
+          });
+
+var s3              = new AWS.S3();
+var date            = new Date().toDateString().replace(/\s/g, '_');
+
+var connection = sql_connect.connect();
+var body = JSON.stringify(DB.reportsDB(connection, 'version1'));
+
+console.log('body', body);
+
+var params = {
+     Bucket: S3_bucket,
+     Body: body,
+     Key: 'roblum/Content_API/reports/' + date + '.json',
+}
+
+s3.upload(params, function(err, data){
+     if (err){
+          console.log(err);
+     } else{
+          console.log(data);
+     }
+});
+
