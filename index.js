c960e775b10c904b94b30a5ba2f14db43a3c1b49
Rob Lum <lum.robert@gmail.com>
2015-01-21 14:51:40 -0500

    testing ssl force

--- index.js
+++ index.js
@@ -4,14 +4,23 @@ var sql_connect     = require('./controllers/sql_connection.js');
 var DB              = require('./controllers/db_queries.js');
 var cors            = require('cors');
 
+var forceSsl = function(req, res, next) {
+   if (req.headers['x-forwarded-proto'] !== 'https') {
+       return res.redirect(['https://', req.get('Host'), req.url].join(''));
+   }
+   return next();
+};
+
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+// app.use(cors());
+app.use(forceSsl);
 
 app.get('/', function(req, res){
      res.send('Opop');
 });
 
-app.post('/report', cors(), function(request, response) {
+app.post('/report', function(request, response) {
      response.send('Hello World!');
 
      var connection = sql_connect.connect();
@@ -24,5 +33,4 @@ app.post('/report', cors(), function(request, response) {
 app.listen(app.get('port'), function() {
      console.log("Node app is running at localhost:" + app.get('port'));
 });
-
 
